b908ccf9ea1d358aef109fc292f8c94899044d2d
svitorroberto <vitorroberto.info@gmail.com>
2016-02-26 17:06:48 -0300

    conexao pronta

---
+++ src/main/conexao/Conexao.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+	String url = "jdbc:mysql://localhost:3306/unificada";
+	String usuario = "root";
+	String senha = "aluno";
+	Connection con;
+
+	public void conectar() {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(url, usuario, senha);
+			System.out.println("Conectado!");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (SQLException e2) {
+			e2.printStackTrace();
+		}
+
+	}
+
+	public void desconectar() {
+		try {
+			con.close();
+			System.out.println("Desconectado!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
