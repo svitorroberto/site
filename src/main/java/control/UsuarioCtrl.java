ecff99f60aa1c488c6f03c2c8237c382a1ba68b4
svitorroberto <vitorroberto.info@gmail.com>
2016-02-26 17:48:17 -0300

    pasta java

--- src/main/java/control/UsuarioCtrl.java
+++ src/main/java/control/UsuarioCtrl.java
@@ -5,10 +5,11 @@ import java.util.List;
 import model.Usuario;
 
 public class UsuarioCtrl {
-	Usuario usuario;
+	
 	List<Usuario> usuarios;
 	
 	public void novoUsuario() {
+		Usuario usuario = new Usuario();
 		
 	}
 }
